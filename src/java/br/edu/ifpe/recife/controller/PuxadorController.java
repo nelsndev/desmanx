fa6a4d09aba743d4111e27dd918bd8e4f1e17629
Nelson Henrique <nhsneto@gmail.com>
2022-12-29 08:20:20 -0300

    Remove importacao desnecessaria

--- src/java/br/edu/ifpe/recife/controller/PuxadorController.java
+++ src/java/br/edu/ifpe/recife/controller/PuxadorController.java
@@ -8,7 +8,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
 
 @ManagedBean(name = "puxadorCtrl")
 @SessionScoped
